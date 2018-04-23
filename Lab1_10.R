5b4267a622dfe0350f3a83cab981461eb03f32bb
Simon94 <szymon7a@gmail.com>
2018-04-23 23:40:33 +0200

    ..

--- Lab1_10.R
+++ Lab1_10.R
@@ -1,36 +1,18 @@
+library(MCDA)
+
 epsilon <-0.01
 
-t = read.table("Data.csv", header = TRUE, sep = ",")
+t = read.table("Data.csv", header = TRUE, sep = ",", row.names = 1)
 
-# the performance table
-performanceTable <- rbind(
-  c(5,2,16,449),
-  c(5.2,2,16,649),
-  c(5.5,3,32,829),
-  c(5.3,4,64,1899)
-)
 
-rownames(performanceTable) <- c(
-  "Nokia 3 Dual Sim ",
-  "Nokia 5 Dual Sim",
-  "Nokia 6 Dual Sim",
-  "Nokia 8"
-)
-colnames(performanceTable) <- c(
-  "Display",
-  "RAM",
-  "ROM",
-  "Price"
-)
 # ranks of the alternatives
 alternativesRanks <- c(1,2,3,4)
-
-names(alternativesRanks) <- row.names(performanceTable)
+names(alternativesRanks) <- row.names(t)
 
 # criteria to minimize or maximize
-criteriaMinMax <- c("max","max","max","min")
-names(criteriaMinMax) <- colnames(performanceTable)
+criteriaMinMax <- c("min","max","max","min")
+names(criteriaMinMax) <- colnames(t)
 
-library(MCDA)
-x<-additiveValueFunctionElicitation(performanceTable, criteriaMinMax, epsilon, alternativesRanks = alternativesRanks)
-
+x<-additiveValueFunctionElicitation(
+  t,criteriaMinMax, epsilon, alternativesRanks = alternativesRanks)
+View
